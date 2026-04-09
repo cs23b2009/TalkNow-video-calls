23bdc6b393dbd6438219f890d2d264ef08c54a0b
indra kumar <147529343+cs23b2009@users.noreply.github.com>
2026-04-09 17:35:40 +0530

    chore: use VITE_BACKEND_URL env variable for axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://talknow-video-calls.onrender.com/api";
+const BASE_URL = import.meta.env.MODE === "development" 
+  ? "http://localhost:5001/api" 
+  : `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
